1f5285f3b69b10f72fe3b0c28ab1cfaf8c479a21
faizuddin <faizsyed14@gmail.com>
2022-11-09 12:08:02 +0530

    minor changes
    Co-Authored-By: feartoast <63493692+feartoast@users.noreply.github.com>

--- pages/index.php
+++ pages/index.php
@@ -1,12 +1,12 @@
 <?php 
 get_comp('pre');
-get_comp('navbar-1'); ?>
+get_comp('navbar'); ?>
 <article>
    <div class="row">
     <div class="col-6">
     <h1>ZORO.AIO</h1>
     <?php
-     
+
     ?>
     </div>
     <div class="col-6"></div>
